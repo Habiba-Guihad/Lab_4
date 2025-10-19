ff43399521c62bc59f073d2bcbb544bd800533fb
hallaelgammal <hallaelgammal6@gmail.com>
2025-10-19 11:28:55 +0300

    Create Product.java

---
+++ src/lab4_1/Product.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4;
+
+/**
+ *
+ * @author Abdallah
+ */
+public class Product {
+    private String productId;
+    private String productName;
+    private String manufacturerName;
+    private String supplierName;
+    private int quantity;
+    private float price;
+    
+    public Product(String productId, String productName, String manufacturerName, String supplierName, int quantity, float price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.manufacturerName = manufacturerName;
+        this.supplierName = supplierName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+    
+    public int getQuantity() {
+        return quantity;
+    }
+    
+    public void setQuantity(int quantity) {
+        this.quantity= quantity;
+    }
+    
+    public  String lineRepresentation() {
+        return productId + "," + productName + "," + manufacturerName + "," +
+               supplierName + "," + quantity + "," + price;
+    }
+    
+    public String getSearchKey() {
+        return productId;
+    }
+    
+    
+}
