93869f5798b531aaaae42329bb63f0f300948889
Habiba-Guihad <es.habiba.khalil2023@alexu.edu.eg>
2025-10-19 14:04:51 +0300

    Create Item.java

---
+++ src/lab4_1/Item.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package lab.pkg4;
+
+
+/**
+ *
+ * @author Gehad
+ */
+public interface Item {
+
+    public String lineRepresentation();
+
+    public String getSearchKey();
+}
