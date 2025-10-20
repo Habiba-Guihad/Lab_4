4785692a0b7ab7a7c77a22d1c7c4bfa15ec8c54c
Hana-Gamaleldin14 <es.hana.ahmed20231@alexu.edu.eg>
2025-10-20 03:38:24 +0300

    Update CustomerProductDatabase.java

--- src/lab4_1/CustomerProductDatabase.java
+++ src/lab4_1/CustomerProductDatabase.java
@@ -3,40 +3,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package lab4_1;
-
-import java.util.ArrayList;
-import java.util.Scanner;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.io.*;
+
 
 public class CustomerProductDatabase extends Database {
 
     public CustomerProductDatabase(String filename) {
         super(filename);
     }
-
-   
-
-//    public void readFromFile() {
-//        records.clear();//3shan at2aked en list empty abl mah 2a2ra
-////        try {
-//            File file = new File(filename);
-//            Scanner scan = new Scanner(file);
-//            while (scan.hasNextLine()) {
-//                String line = scan.nextLine();
-//                CustomerProduct custprod = createRecordFrom(line);
-//                if (custprod != null) {
-//                    records.add(custprod);
-//                }
-//            }
-//            scan.close();
-//            System.out.println("Customer Product File read successfully");
-//        } catch (FileNotFoundException e) {
-//            System.out.println("Error file not found!");
-//        }
-//    }
-
     @Override
     public Item createRecordFrom(String line) {
         String[] parts = line.split(",");
@@ -57,56 +32,5 @@ public class CustomerProductDatabase extends Database {
             System.out.println("Invalid line" + line);
             return null;
         }
-    }
-
- 
-
-//    public void insertRecord(CustomerProduct record) {
-//        if (record == null) {
-//            System.out.println("There is no record");
-//            return;
-//        }
-//        if (contains(record.getSearchKey())) {
-//            System.out.println("Record already exist");
-//        } else {
-//            records.add(record);
-//            System.out.println("Record added successfully");
-//        }
-//    }
-
-//    public void deleteRecord(String key) {
-//        boolean found = false;
-//        for (int i = 0; i < records.size(); i++) {
-//            CustomerProduct custmprod = records.get(i);
-//            if (custmprod.getSearchKey().equals(key)) {
-//                records.remove(i);
-//                System.out.println("Record with key  " + key + " removed successfully");
-//                found = true;
-//                break;
-//            }
-//        }
-//        if (!found) {
-//            System.out.println("No record found with key  " + key);
-//        }
-//    }
-
-//    public void saveToFile() {
-//        try {
-//            FileWriter writer = new FileWriter(filename, false);//false di=over write old file
-//            for (int i = 0; i < records.size(); i++) {
-//                CustomerProduct custmprod = records.get(i);
-//                writer.write(custmprod.lineRepresentation());
-//                if (i < records.size() - 1) {
-//                    writer.write("\n");//to add new line
-//                }
-//            }
-//            writer.close();
-//            System.out.println("Data saved successfully to " + filename);
-//        } catch (IOException e)//input aw output exception w el e heya object mn no3 exception
-//        {
-//            System.out.println("Error writing to file: " + e.getMessage());
-//        }
-//    }
-    
-     
+    }        
 }
