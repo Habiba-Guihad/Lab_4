092f7041019ab397beca185d8e046bd17285f188
Habiba-Guihad <es.habiba.khalil2023@alexu.edu.eg>
2025-10-20 03:26:36 +0300

    Update EmployeeUserDatabase.java

--- src/lab4_1/EmployeeUserDatabase.java
+++ src/lab4_1/EmployeeUserDatabase.java
@@ -11,39 +11,37 @@ import java.io.*;
  *
  * @author Dell
  */
-class EmployeeUserDatabase
+class EmployeeUserDatabase extends Database
 {
-    private ArrayList<EmployeeUser> records;
-    private String filename;
-    public EmployeeUserDatabase(String filename)
-    {
-        this.filename=filename;
-        this.records=new ArrayList<>();
+
+    public EmployeeUserDatabase(String filename) {
+        super(filename);
     }
-    public void readFromFile()
-    {
-       records.clear();//3shan at2aked en list empty abl mah 2a2ra
-       try
-       {
-           File file=new File(filename);
-           Scanner scan=new Scanner(file);
-           while(scan.hasNextLine())
-           {
-               String line=scan.nextLine();
-               EmployeeUser employee=createRecordFrom(line);
-               if(employee!=null)
-               {
-                   records.add(employee);
-               }
-           }
-           scan.close();
-           System.out.println("File read successfully");
-       }
-       catch(FileNotFoundException e)
-       {
-           System.out.println("Error file not found!");
-       }
-    }
+  
+//    public void readFromFile()
+//    {
+//       records.clear();//3shan at2aked en list empty abl mah 2a2ra
+//       try
+//       {
+//           File file=new File(filename);
+//           Scanner scan=new Scanner(file);
+//           while(scan.hasNextLine())
+//           {
+//               String line=scan.nextLine();
+//               EmployeeUser employee=createRecordFrom(line);
+//               if(employee!=null)
+//               {
+//                   records.add(employee);
+//               }
+//           }
+//           scan.close();
+//           System.out.println("File read successfully");
+//       }
+//       catch(FileNotFoundException e)
+//       {
+//           System.out.println("Error file not found!");
+//       }
+//    }
     public EmployeeUser createRecordFrom(String line)
     {
         String[] parts=line.split(",");
@@ -57,84 +55,61 @@ class EmployeeUserDatabase
             return null;
         }
     }
-    public ArrayList<EmployeeUser> returnAllRecords()
-    {
-        return records;
-    }
-    public boolean contains(String key )
-    {
-        for(int i=0;i<records.size();i++)
-        {
-            EmployeeUser employee=records.get(i);
-            if(employee.getSearchKey().equals(key))
-                return true;
-        }
-        return false;
-    }
-    public EmployeeUser getRecord(String key)
-    {
-        for(int i=0;i<records.size();i++)
-        {
-            EmployeeUser employee=records.get(i);
-            if(employee.getSearchKey().equals(key))
-                return employee;
-        }
-        return null;
-    }
-    public void insertRecord(EmployeeUser record)
-    {
-        if(record==null)
-        {
-            System.out.println("There is no record");
-            return;
-        }
-        if(contains(record.getSearchKey()))
-        {
-            System.out.println("This employee with id" +record.getSearchKey()+ "already exist");
-        }
-        else
-        {
-            records.add(record);
-            System.out.println("Employee added successfully");
-        }
-    }
-    public void deleteRecords(String key)
-    {
-        boolean found=false;
-        for(int i=0;i<records.size();i++)
-        {
-            EmployeeUser employee=records.get(i);
-            if(employee.getSearchKey().equals(key))
-            {
-                records.remove(i);
-                System.out.println("Employee with id "+key+" removed successfully");
-                found=true;
-                break;
-            }
-        }
-        if(!found)
-        {
-            System.out.println("No employee found with id "+key);
-        }
-    }
-    public void saveToFile()
-    {
-        try
-        {
-            FileWriter writer=new FileWriter(filename,false);//false di=over write old file
-            for(int i=0;i<records.size();i++)
-            {
-                EmployeeUser employee=records.get(i);
-                writer.write(employee.lineRepresentation());
-                if(i<records.size()-1)
-                    writer.write("\n");//to add new line
-            }
-            writer.close();
-            System.out.println("Data saved successfully");
-        }
-        catch(IOException e)//input aw output exception w el e heya object mn no3 exception
-        {
-           System.out.println("Error writing to file: " +e.getMessage()); 
-        }
-    }
+    
+//    public void insertRecord(EmployeeUser record)
+//    {
+//        if(record==null)
+//        {
+//            System.out.println("There is no record");
+//            return;
+//        }
+//        if(contains(record.getSearchKey()))
+//        {
+//            System.out.println("This employee with id" +record.getSearchKey()+ "already exist");
+//        }
+//        else
+//        {
+//            records.add(record);
+//            System.out.println("Employee added successfully");
+//        }
+//    }
+//    public void deleteRecords(String key)
+//    {
+//        boolean found=false;
+//        for(int i=0;i<records.size();i++)
+//        {
+//            EmployeeUser employee=records.get(i);
+//            if(employee.getSearchKey().equals(key))
+//            {
+//                records.remove(i);
+//                System.out.println("Employee with id "+key+" removed successfully");
+//                found=true;
+//                break;
+//            }
+//        }
+//        if(!found)
+//        {
+//            System.out.println("No employee found with id "+key);
+//        }
+//    }
+//    public void saveToFile()
+//    {
+//        try
+//        {
+//            FileWriter writer=new FileWriter(filename,false);//false di=over write old file
+//            for(int i=0;i<records.size();i++)
+//            {
+//                EmployeeUser employee=records.get(i);
+//                writer.write(employee.lineRepresentation());
+//                if(i<records.size()-1)
+//                    writer.write("\n");//to add new line
+//            }
+//            writer.close();
+//            System.out.println("Data saved successfully");
+//        }
+//        catch(IOException e)//input aw output exception w el e heya object mn no3 exception
+//        {
+//           System.out.println("Error writing to file: " +e.getMessage()); 
+//        }
+//    }
 }
